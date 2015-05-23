514c7908adf68bb1406781929d6e9d835b51f4f8
Jürgen Urban <JuergenUrban@gmx.de>
2015-05-23 18:48:36 +0200

    python-007-disable-extensions.patch

--- smp86xx_toolchain_2.8.2.0/toolchain_build_mipsel_nofpu/Python-2.7.8/setup.py
+++ smp86xx_toolchain_2.8.2.0/toolchain_build_mipsel_nofpu/Python-2.7.8/setup.py
@@ -33,7 +33,10 @@ host_platform = get_platform()
 COMPILED_WITH_PYDEBUG = ('--with-pydebug' in sysconfig.get_config_var("CONFIG_ARGS"))
 
 # This global variable is used to hold the list of modules to be disabled.
-disabled_module_list = []
+try:
+    disabled_module_list = sysconfig.get_config_var("DISABLED_EXTENSIONS").split(" ")
+except KeyError:
+    disabled_module_list = list()
 
 def add_dir_to_list(dirlist, dir):
     """Add the directory 'dir' to the list 'dirlist' (at the front) if
